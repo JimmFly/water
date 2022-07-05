ad74f7ae26d6d709c4ecf30416419ef6a02423e5
Fly Gor <447268514@qq.com>
2022-07-05 11:08:20 +0800

    添加时钟

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Clock from "./pages/Clock";
 
 function App() {
   return (
     <div className="App">
+      <Clock />
       <Home />
     </div>
   );
