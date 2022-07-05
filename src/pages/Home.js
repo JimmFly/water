4c4360879132719a4e317359394d3d138764a2fc
Fly Gor <447268514@qq.com>
2022-07-05 10:39:38 +0800

    添加大水杯加水功能

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,50 +6,70 @@ const Home = () => {
     isFull: false,
   }));
   const [list, setList] = useState(data);
+  const [capacity, setCapacity] = useState(0);
 
   // 点击小水杯加水
-  const handleCup = (e) => {
+  const fullCup = (e) => {
     setList(() => {
       let newList = [...list];
-      if (newList[e - 1].isFull === false) {
-        for (let i = 0; i < e; i++) {
-          newList[i].isFull = true;
-        }
-      } else if (newList[e - 1].isFull === true) {
-        for (let i = 7; i > e - 1; i--) {
-          newList[i].isFull = false;
-        }
-      } else {
+      for (let i = 0; i < e; i++) {
+        newList[i].isFull = true;
       }
-
       return newList;
     });
+    setCapacity(e);
   };
+  // 点击满杯倒水
+  const emptyCup = (e) => {
+    setList(() => {
+      let newList = [...list];
+      for (let i = 7; i > e - 2; i--) {
+        newList[i].isFull = false;
+      }
+      return newList;
+    });
+    setCapacity(e - 1);
+  };
+
   // 点击按钮重置小水杯
   const reset = () => {
     setList(() => {
       let newList = data;
       return newList;
     });
+    setCapacity(0);
   };
   return (
     <div className="water">
       <h1>提醒喝水器</h1>
       <h3>目标：2L</h3>
       <div className="cup">
-        <div className="remained" id="remained">
-          <span id="liters"></span>
-          <small>剩余</small>
-        </div>
-
-        <div className="percentage" id="percentage"></div>
+        {capacity === "8" ? null : (
+          <div className="remained" id="remained">
+            <span id="liters"></span>
+            <small>剩余{(2 * (100 - capacity * 12.5)) / 100}L</small>
+          </div>
+        )}
+        {capacity === 0 ? null : (
+          <div
+            className="percentage"
+            id="percentage"
+            style={{ height: `${capacity * 32.5}px ` }}
+          >
+            {capacity * 12.5}%
+          </div>
+        )}
       </div>
       <p className="text">选择你喝了几杯水</p>
       <div className="cups">
         {list.map((item) => {
           return (
             <div
-              onClick={(e) => handleCup(e.target.id)}
+              onClick={
+                item.isFull
+                  ? (e) => emptyCup(e.target.id)
+                  : (e) => fullCup(e.target.id)
+              }
               className={item.isFull ? "cup cup-small full" : "cup cup-small"}
               id={item.id}
               key={item.id}
